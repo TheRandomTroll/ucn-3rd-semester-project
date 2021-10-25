50dda15058963dbbf1f449caabada60b1e060b55
TheRandomTroll <mihailgerginov01@gmail.com>
2021-10-25 15:54:51 +0200

    Add project files.

---
+++ StreetPatch.Data/StreetPatchDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using StreetPatch.Data.Entities;
+
+namespace StreetPatch.Data
+{
+    public class StreetPatchDbContext : IdentityDbContext<ApplicationUser, IdentityRole, string>
+    {
+        public StreetPatchDbContext(DbContextOptions<StreetPatchDbContext> options)
+            : base(options)
+        {
+            
+        }
+    }
+}
