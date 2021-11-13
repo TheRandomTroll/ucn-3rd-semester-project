f0244b315eb4beb701151f61f919043175210e77
TheRandomTroll <mihailgerginov01@gmail.com>
2021-11-13 15:35:19 +0100

    Added report class; started work on comment class; TODO after upgrade to .NET 6

---
+++ StreetPatch.Data/Entities/Comment.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using StreetPatch.Data.Entities.Base;
+
+namespace StreetPatch.Data.Entities
+{
+    public class Comment : EntityBase
+    {
+        public string Content { get; set; }
+    }
+}
