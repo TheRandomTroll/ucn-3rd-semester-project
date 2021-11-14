55968655cfc1b1a85173eed536b4e6f0b4fe742f
TheRandomTroll <mihailgerginov01@gmail.com>
2021-11-14 18:24:59 +0100

    Implemented Repository pattern. To be tested

---
+++ StreetPatch.Data/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StreetPatch.Data.Entities.Base;
+
+namespace StreetPatch.Data.Repositories.Interfaces
+{
+    public interface IRepository<T> where T : EntityBase
+    {
+        Task<List<T>> GetAllAsync();
+        Task<T> GetAsync(Guid id);
+        Task<T> AddAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task<T> DeleteAsync(Guid id);
+    }
+}
