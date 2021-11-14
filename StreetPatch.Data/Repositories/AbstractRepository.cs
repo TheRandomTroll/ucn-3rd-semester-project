1baf48ca6c6878d1ffc94bef3d0ecec7f0b263e4
Mihail Gerginov <mihailgerginov01@gmail.com>
2021-11-14 18:32:32 +0100

    Implemented Repository pattern. To be tested (#4)

---
+++ StreetPatch.Data/Repositories/AbstractRepository.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using StreetPatch.Data.Entities.Base;
+using StreetPatch.Data.Repositories.Interfaces;
+
+namespace StreetPatch.Data.Repositories
+{
+    public abstract class AbstractRepository<TEntity, TContext> : IRepository<TEntity>
+        where TEntity : EntityBase
+        where TContext : DbContext
+    {
+        private readonly TContext context;
+
+        protected AbstractRepository(TContext context)
+        {
+            this.context = context;
+        }
+        public async Task<TEntity> AddAsync(TEntity entity)
+        {
+            context.Set<TEntity>().Add(entity);
+            await context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<TEntity> DeleteAsync(Guid id)
+        {
+            var entity = await context.Set<TEntity>().FindAsync(id);
+            if (entity == default(TEntity))
+            {
+                return default;
+            }
+
+            context.Set<TEntity>().Remove(entity);
+            await context.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public async Task<TEntity> GetAsync(Guid id)
+        {
+            return await context.Set<TEntity>().FindAsync(id);
+        }
+
+        public async Task<List<TEntity>> GetAllAsync()
+        {
+            return await context.Set<TEntity>().ToListAsync();
+        }
+
+        public async Task<TEntity> UpdateAsync(TEntity entity)
+        {
+            context.Entry(entity).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+            return entity;
+        }
+
+    }
+}
