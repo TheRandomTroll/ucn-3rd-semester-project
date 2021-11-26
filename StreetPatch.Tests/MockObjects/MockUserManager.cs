6bb80038e04e1537c5d30caf51be168c178b622b
Mihail Gerginov <mihailgerginov01@gmail.com>
2021-11-26 17:41:48 +0100

    Added missing unit tests (#16)
    Co-authored-by: TheRandomTroll <mihail.gerginov@gogift.com>

---
+++ StreetPatch.Tests/MockObjects/MockUserManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using StreetPatch.Data.Entities;
+
+namespace StreetPatch.Tests.MockObjects
+{
+    public class MockUserManager : UserManager<ApplicationUser>
+    {
+        public MockUserManager()
+            : base(new Mock<IUserStore<ApplicationUser>>().Object,
+                new Mock<IOptions<IdentityOptions>>().Object,
+                new Mock<IPasswordHasher<ApplicationUser>>().Object,
+                Array.Empty<IUserValidator<ApplicationUser>>(),
+                Array.Empty<IPasswordValidator<ApplicationUser>>(),
+                new Mock<ILookupNormalizer>().Object,
+                new Mock<IdentityErrorDescriber>().Object,
+                new Mock<IServiceProvider>().Object,
+                new Mock<ILogger<UserManager<ApplicationUser>>>().Object)
+        {
+        }
+    }
+}
